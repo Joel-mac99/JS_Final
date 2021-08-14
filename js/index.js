a8df54d253ad78bc7b42f832c204c4bb9a739900
Dami Hundeyin <d.hundeyin@yahoo.com>
2021-08-14 10:15:45 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// Data for the rooms
+const roomsData = [
+    {
+        id: 1,
+        name: 'Double Room',
+        price: 199,
+        size: 45,
+        capacity: 4,
+        bedSize: 'Double',
+        services: ['Wifi', 'Television', 'Bathroom', 'Breakfast', 'Kettles', 'Toaster', 'Microwave', 'Safe', 'Hair Dryer', 'Fridge'],
+        images: ['https://cdn.traveltripper.io/site-assets/512_863_12597/media/2018-02-22-041437/large_DDBDB.jpg', 'https://lh6.googleusercontent.com/LAbDs9JrwGjxAAQoHT1lNnKt4a5_OXDqw7S3hZQajGS30XvPwjynPOA8-afixA6QqxL_g7iPn12dRWTO40GhEyPS7n7CDzRYg61Ib334yCK6ZIPjLNZbZExrLk_8BPvf83fq1Qte', 'https://cdn.traveltripper.io/site-assets/512_855_12327/media/2018-02-27-080006/large_ex-double-2.jpg'],
+        securityDeposit: 50
+    },
+    {
+        id: 2,
+        name: 'Premium King Room',
+        price: 159,
+        size: 30,
+        capacity: 2,
+        bedSize: 'King',
+        services: ['Wifi', 'Television', 'Bathroom', 'Breakfast', 'Kettles', 'Toaster', 'Microwave', 'Safe', 'Fridge', 'Room Service'],
+        images: ['https://cdn.galaxy.tf/unit-media/tc-default/uploads/images/room_photo/001/623/426/sutton-place-s1k-01-wide.jpg', 'https://www.pantageshotel.com/wp-content/uploads/2019/06/PantagesApril5-11a-1-1600x1119.jpg', 'https://blog.travala.com/wp-content/uploads/2020/03/blockchain-revolution-1.jpg'],
+        securityDeposit: 100
+    },
+    {
+        id: 3,
+        name: 'Deluxe Room',
+        price: 250,
+        size: 60,
+        capacity: 4,
+        bedSize: 'King',
+        services: ['Wifi', 'Television', 'Bathroom', 'Breakfast', 'Kettles', 'Toaster', 'Microwave', 'Safe'],
+        images: ['https://www.plazahotelcasino.com/wp-content/uploads/2014/11/DeluxeRoom-Hero-1.jpg', 'https://www.plazahotelcasino.com/wp-content/uploads/2019/02/MiniSuite-Hero-768x339.jpg'],
+        securityDeposit: 150
+    }
+]
